24ad8c42e534fddf67121f6e92819264df5f9c18
Tak <naclsoltakumi@gmail.com>
2021-03-11 08:00:27 +0900

    [add]デバッグ用関数群のモジュールを追加

---
+++ Source/debug.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 11 05:50:57 2021
+
+@author: Takumi.H
+
+This module is a set of functions used for debugging.
+"""
+
+import cv2
+import numpy as np
+
+def show_detected_Item( image, contours ):
+    """
+    認識した対象物の境界線を元画像データの上に描画し、別ウィンドウに表示する関数。
+    元画像データには変更を加えない。
+    引数 image:元の画像データ contours:輪郭座標のリスト
+    """
+
+    image_debug = numpy.copy(image)
+    for contour in contours:
+        if len(contour) > 0:
+            rect = contour
+            x, y, w, h = cv2.boundingRect(rect)
+            cv2.rectangle(image_debug, (x, y), (x + w, y + h), (0, 255, 0), 10)
+
+    cv2.imshow('image', image_debug)
